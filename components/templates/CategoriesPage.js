311e1a054fef0bf3161dbf682f5fd71c702e719c
Vahid Hosseini <vhdnet@gmail.com>
2025-09-18 16:43:55 +0330

    feat(categories): handle select inputs and build query params in URL

--- components/templates/CategoriesPage.js
+++ components/templates/CategoriesPage.js
@@ -1,28 +1,49 @@
+import { useState } from "react";
 import styles from "./CategoriesPage.module.css";
+import { useRouter } from "next/router";
 
 function CategoriesPage() {
-  return <div className={styles.container} >
-    <h2>Categories</h2>
-    <div className={styles.subContainer}>
+  const router = useRouter();
+  const [query, setQuery] = useState({ difficulty: "", time: "" });
+
+  const changeHandler = (e) => {
+    setQuery({ ...query, [e.target.name]: e.target.value });
+  };
+
+  const searchHandler = () => {
+    router.push({
+      pathname: "/categories",
+      query,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>Categories</h2>
+      <div className={styles.subContainer}>
         <div className={styles.select}>
-          <select>
-            <option>Difficulty</option>
-            <option>Easy</option>
-            <option>Medium</option>
-            <option>Hard</option>
+          <select
+            value={query.difficulty}
+            name="difficulty"
+            onChange={changeHandler}
+          >
+            <option value="">Difficulty</option>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
           </select>
 
-          <select>
-            <option>Cooking Time</option>
-            <option>More than 30mins</option>
-            <option>less than 30 min</option>
+          <select value={query.time} name="time" onChange={changeHandler}>
+            <option value="">Cooking Time</option>
+            <option value="more">More than 30mins</option>
+            <option value="less">less than 30 min</option>
           </select>
 
-          <button>Search</button>
-
+          <button onClick={searchHandler}>Search</button>
         </div>
+      </div>
     </div>
-  </div>
+  );
 }
 
 export default CategoriesPage;
